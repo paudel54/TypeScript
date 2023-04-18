e6e5299b72480fade95a5f079e3567fbff065c7f
paudel54 <2sansrit@gmail.com>
2023-04-18 13:02:56 +0545

    defined type and checked use case of + operand

--- TS_Basics/2_/app.js
+++ TS_Basics/2_/app.js
@@ -1,1 +1,16 @@
-console.log('code generating space ultra really');
+// console.log('code generating space ultra really');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        //string + number + number => String;
+        console.log(n1 + n2 + 'hello');
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 4.6;
+var printResult = true;
+var phrase = 'Total Sum ->';
+add(number1, number2, printResult, phrase);
--- TS_Basics/2_/app.ts
+++ TS_Basics/2_/app.ts
@@ -1,1 +1,19 @@
-console.log('code generating space ultra really');
+// console.log('code generating space ultra really');
+
+function add (n1:number,n2:number, showResult:boolean, phrase:string){
+
+    const result = n1+n2;
+    if(showResult){
+        //string + number + number => String;
+        console.log(phrase+result);
+    }else{
+        return n1+n2;
+    }
+
+}
+const number1 = 5;
+const number2 = 4.6;
+const printResult = true;
+const phrase= 'Total Sum ->'
+
+add(number1, number2, printResult, phrase);
