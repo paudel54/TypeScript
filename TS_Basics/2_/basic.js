1305f28582c385eaf7a32ce97fd9986e89d5596b
paudel54 <2sansrit@gmail.com>
2023-04-19 10:22:34 +0545

    Clearified use case if enum

--- TS_Basics/2_/basic.js
+++ TS_Basics/2_/basic.js
@@ -10,20 +10,47 @@
 // };
 // console.log('She is a lady and her age is', Person.age);
 //Normal object defination
-var Person = {
-    name: 'sansrit',
-    age: 27,
-    hobbies: ['Sports', 'Music', 'Cooking'],
-    role: [2, 'author']
-};
+// let Person:{
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     // role is  a tuple: tuple is speical kind of array, where total number of element is predefined, along with their types.
+//     //downside of it is array.push() still works onto it.  
+//     role: [number, string]
+// } = {
+//     name: 'sansrit',
+//     age: 27,
+//     hobbies: ['Sports', 'Music', 'Cooking'],
+//     role: [2, 'author']
+// };
 //adding to tuple, role
-Person.role.push('admin');
-console.log('this is current role', Person.role);
-//to defined arrya of mixed type write :[any]
-var favouriteAct;
-favouriteAct = ['Swimming, Music'];
-console.log(Person.name);
-for (var _i = 0, _a = Person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+// Person.role.push('admin')
+// //tuple downside still array.push works
+// // console.log('this is current role', Person.role);
+// //to defined arrya of mixed type write :[any]
+// let favouriteAct : string[];
+// favouriteAct = ['Swimming, Music'];
+// console.log(Person.name);
+// for(const hobby of Person.hobbies){
+//     console.log(hobby.toUpperCase())
+// }
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+//behind the scene admin, get 0 read_only 1 and author 2
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'Hari Krishan',
+    age: 30,
+    hobbies: ['Sport', 'Drama'],
+    role: Role.ADMIN
+};
+console.log('The value of Author enum', Role.AUTHOR);
+if (person.role === Role.AUTHOR) {
+    console.log('Is admin');
 }
--- TS_Basics/2_/basic.ts
+++ TS_Basics/2_/basic.ts
@@ -13,31 +13,50 @@
 // console.log('She is a lady and her age is', Person.age);
 
 //Normal object defination
-let Person:{
-    name: string,
-    age: number,
-    hobbies: string[],
-    // role is  a tuple: tuple is speical kind of array, where total number of element is predefined, along with their types.
-    //downside of it is array.push() still works onto it.  
-    role: [number, string]
-} = {
-    name: 'sansrit',
-    age: 27,
-    hobbies: ['Sports', 'Music', 'Cooking'],
-    role: [2, 'author']
-};
+// let Person:{
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     // role is  a tuple: tuple is speical kind of array, where total number of element is predefined, along with their types.
+//     //downside of it is array.push() still works onto it.  
+//     role: [number, string]
+// } = {
+//     name: 'sansrit',
+//     age: 27,
+//     hobbies: ['Sports', 'Music', 'Cooking'],
+//     role: [2, 'author']
+// };
 //adding to tuple, role
 
-Person.role.push('admin')
-//tuple downside still array.push works
-// console.log('this is current role', Person.role);
+// Person.role.push('admin')
+// //tuple downside still array.push works
+// // console.log('this is current role', Person.role);
 
-//to defined arrya of mixed type write :[any]
-let favouriteAct : string[];
-favouriteAct = ['Swimming, Music'];
-console.log(Person.name);
+// //to defined arrya of mixed type write :[any]
+// let favouriteAct : string[];
+// favouriteAct = ['Swimming, Music'];
+// console.log(Person.name);
 
-for(const hobby of Person.hobbies){
-    console.log(hobby.toUpperCase())
+// for(const hobby of Person.hobbies){
+//     console.log(hobby.toUpperCase())
+// }
+    
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+//behind the scene value of enum is auto allocated admin, get 0 read_only 1 and author 2
+//we can initialize enum value with any starting value.  or assign custom value with any string number anyting, default is number
+enum Role {ADMIN, READ_ONLY, AUTHOR}
+
+const person = {
+    name: 'Hari Krishan',
+    age: 30,
+    hobbies: ['Sport', 'Drama'],
+    role: Role.ADMIN
+} 
+
+console.log('The value of Author enum',Role.AUTHOR)
+
+if(person.role === Role.AUTHOR){
+    console.log('Is admin');
 }
-
