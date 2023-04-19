a793f8fb670efe49e2af4ba4b2ec65127f4dba0f
paudel54 <2sansrit@gmail.com>
2023-04-19 07:37:03 +0545

    Added TS with Strings

--- TS_Basics/2_/basic.js
+++ TS_Basics/2_/basic.js
@@ -1,13 +1,24 @@
 // console.log('Hello i am from basic TS');
 //object type in TS
 //method 1
-var Person = {
-    name: 'Prakiti',
-    age: 30
-};
-console.log('She is a lady and her age is', Person.age);
+// const Person:{
+//     name:string;
+//     age: number;
+// }={
+//     name: 'Prakiti',
+//     age:30
+// };
+// console.log('She is a lady and her age is', Person.age);
 //Normal object defination
-// let ordinaryPerson = {
-//     name: 'sansrit',
-//     age: '27'
-// }
+var Person = {
+    name: 'sansrit',
+    age: '27',
+    hobbies: ['Sports', 'Music', 'Cooking']
+};
+var favouriteAct;
+favouriteAct = ['Swimming, Music'];
+console.log(Person.name);
+for (var _i = 0, _a = Person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- TS_Basics/2_/basic.ts
+++ TS_Basics/2_/basic.ts
@@ -2,19 +2,29 @@
 
 //object type in TS
 //method 1
-const Person:{
-    name:string;
-    age: number;
-}={
-    name: 'Prakiti',
-    age:30
-};
+// const Person:{
+//     name:string;
+//     age: number;
+// }={
+//     name: 'Prakiti',
+//     age:30
+// };
 
-console.log('She is a lady and her age is', Person.age);
+// console.log('She is a lady and her age is', Person.age);
 
 //Normal object defination
-// let ordinaryPerson = {
-//     name: 'sansrit',
-//     age: '27'
-// }
+let Person = {
+    name: 'sansrit',
+    age: '27',
+    hobbies: ['Sports', 'Music', 'Cooking']
+};
+
+//to defined arrya of mixed type write :[any]
+let favouriteAct : string[];
+favouriteAct = ['Swimming, Music'];
+console.log(Person.name);
+
+for(const hobby of Person.hobbies){
+    console.log(hobby.toUpperCase())
+}
 
