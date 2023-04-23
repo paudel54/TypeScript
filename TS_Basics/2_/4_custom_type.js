36cbe773918cca5f1b6d4143331d4717143f9d00
paudel54 <2sansrit@gmail.com>
2023-04-23 12:06:53 +0545

    added function return type void

--- TS_Basics/2_/4_custom_type.js
+++ TS_Basics/2_/4_custom_type.js
@@ -1,21 +1,16 @@
- //custom types: 
+//custom types: 
 //  type Combineable = number | string;
 //  type ConversionDescriptor = 'as-number' | 'as-text';
-
 //  function combine(input1:Combineable,input2:Combineable){
 //         let result = 0;
 //         if(typeof input1 === 'number' && typeof input2 === 'number'){
 //             result = input1 + input2;
 //         }
 //  }
-
-//void vs undefined. void has no value undefined have value that doesn't exist
-function add(n1:number, n2:number){
-    return n1+n2;
+function add(n1, n2) {
+    return n1 + n2;
 }
-
-function printResult(num:number):void{
-    console.log('Result: ', num)
+function printResult(num) {
+    console.log('Result: ', num);
 }
-
-printResult(add(10,2));
+printResult(add(10, 2));
