b0649ee8231d83faa89afa6cb9850276ef5b4480
paudel54 <2sansrit@gmail.com>
2023-04-23 18:42:15 +0545

    added multiple TS compilation functions

---
+++ TS_Basics/3_/34_watch.js
@@ -0,0 +1,9 @@
+"use strict";
+//using watch Module::
+// tell TypeScript to watch file on change recompile
+// add -w on end of file
+const userName = 'Sansrit';
+let surName = 'Paudel';
+let add = 'text';
+console.log(userName);
+//Compiling entire multiple projects:35
---
+++ TS_Basics/3_/35_analytics.ts
@@ -0,0 +1,7 @@
+// multiple watch at once 
+//goto correct folder
+//step-1 tsc --init
+//step-2 type tsc. it will compile all the files onto the folder.
+//step-3 tsc -w
+
+console.log(' On Test system! QC passed')
