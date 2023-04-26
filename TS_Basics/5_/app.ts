66a0962dbccf2f122fb8dc12a477a8fcca2bf5dc
paudel54 <2sansrit@gmail.com>
2023-04-26 07:16:44 +0545

    implemented this keyword behaviour with TS

--- TS_Basics/5_/app.ts
+++ TS_Basics/5_/app.ts
@@ -2,13 +2,24 @@
 //class convention to use UpperCase
 //object consiste of key value pair class dont have:
 class Department {
+    //class property
     name:string;
-
     constructor(n:string){
         // fn tied to class and executed when object being craeated good for initialization
         this.name = n;
+    }
+    // method-inside Class:
+    // adding department implies that the this should always refer to instance of department class:
+    describe(this:Department){
+        //name wont' work here, req this
+        console.log('Department: name' + this.name)
     }
 }
 
 const accounting = new Department('Accounting');
 console.log(accounting);
+accounting.describe();
+//accounting.describe points to accountingCopy this refers to current obj
+const accountingCopy = {name: 'Type Department',describe: accounting.describe};
+//general rule of thumb this refers to any obj linked with method. here describe() is linked to accountinCopy where name is undefined.
+accountingCopy.describe();
