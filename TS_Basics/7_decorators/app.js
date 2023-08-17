fa8ab64073fcd232896e127d1676d7d208d15148
paudel54 <2sansrit@gmail.com>
2023-08-17 17:38:04 +0545

    Decorators advance concept walkthrough

---
+++ TS_Basics/7_decorators/app.js
@@ -0,0 +1,8 @@
+var Person = /** @class */ (function () {
+    function Person() {
+        console.log('Creating person object..');
+    }
+    return Person;
+}());
+var pers = new Person();
+console.log(pers);
---
+++ TS_Basics/7_decorators/app.ts
@@ -0,0 +1,19 @@
+
+// Decorators 
+// function Logger(constructor: Function){
+//     console.log('Logging.....');
+//     console.log(constructor);
+// }
+
+// @Logger
+// class Person {
+//     name = 'Max';
+//     constructor(){
+//         console.log('Creating person object..');
+//     }
+// }
+
+// const pers = new Person();
+//=> Provide extra functionality to class. 
+
+
